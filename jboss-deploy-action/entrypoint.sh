eb80d0925c39f257b08135b3542bb644c90f86a2
Jonathan Egger <jonathan.egger@raymondjames.com>
2021-03-22 16:19:03 -0400

    debug

--- jboss-deploy-action/entrypoint.sh
+++ jboss-deploy-action/entrypoint.sh
@@ -3,4 +3,4 @@ sh -c "echo Hello world my name is $INPUT_MY_NAME"
 pwd
 ls
 ls -l /app
-java -cp /app/jbossdeployer.jar com.microsoft.alm.Driver
+java -cp /app/jbossdeployer.jar com.microsoft.alm.Driver -s http-remoting://vrdldap96944.rjf.com:29990 -u admin -p admin123 
