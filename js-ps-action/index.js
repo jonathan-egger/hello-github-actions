f7463fc1b6865687e57fdefc40858a4f047c3f2e
Jonathan Egger <jonathan.egger@raymondjames.com>
2021-03-22 17:17:40 -0400

    debug

--- js-ps-action/index.js
+++ js-ps-action/index.js
@@ -1,20 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const shell = require('node-powershell');
-let ps = new shell({
-  executionPolicy: 'Bypass',
-  noProfile: true
-});
-
-ps.addCommand('echo node-powershell')
-ps.invoke().then(output => {
-  console.log(output);
-}).catch(err => {
-  console.log(err);
-  ps.dispose();
-});
-
-
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -25,6 +11,20 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  let ps = new shell({
+    executionPolicy: 'Bypass',
+    noProfile: true
+  });
+  
+  ps.addCommand('echo node-powershell')
+  ps.invoke().then(output => {
+    console.log(output);
+  }).catch(err => {
+    console.log(err);
+    ps.dispose();
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
