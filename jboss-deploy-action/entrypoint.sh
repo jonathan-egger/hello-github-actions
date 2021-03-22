023184c30ee440f09b63743acd93b450fc5e9204
Jonathan Egger <jonathan.egger@raymondjames.com>
2021-03-22 14:56:06 -0400

    debug

--- jboss-deploy-action/entrypoint.sh
+++ jboss-deploy-action/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -l
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
-java -cp /app/jbossdeployer-0.1.0-jar-with-dependencies.jar com.microsoft.alm.Driver
+pwd
+ls
+ls -l /app
+java -cp /app/jbossdeployer.jar com.microsoft.alm.Driver
